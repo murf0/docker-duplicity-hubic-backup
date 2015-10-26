cd63d3fa2ae2854d4bd115111bcc32d50fba1b75
Mikael Murf Mellgren <mikael@murf.se>
2015-10-26 22:50:57 +0100

    Segfaults

--- duplicity.sh
+++ duplicity.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-set -ex
+#set -ex
 if [ ! -e ~/.hubic_credentials ]; then
 	#build .hubic_credentials
 	echo "[hubic]" > ~/.hubic_credentials
@@ -10,11 +10,9 @@ if [ ! -e ~/.hubic_credentials ]; then
 	echo "redirect_uri = $redirect_uri" >> ~/.hubic_credentials
 fi
 
-export SIGN_PASSPHRASE=$PASSPHRASE
 export HOSTNAME=$TUTUM_NODE_HOSTNAME
 
-cd /BACKUP
-duplicity --verbosity notice --full-if-older-than 14D --num-retries 3 --asynchronous-upload --volsize 100 --allow-source-mismatch ${EXCLUDES}  "./" "cf+hubic://zBKP_${HOSTNAME}"
+duplicity --verbosity notice --full-if-older-than 14D --num-retries 3 --asynchronous-upload --volsize 100 --allow-source-mismatch ${EXCLUDES}  "/BACKUP" "cf+hubic://zBKP_${HOSTNAME}"
 duplicity remove-older-than 6M --force "cf+hubic://zBKP_${HOSTNAME}"
 
 export PASSPHRASE=""
